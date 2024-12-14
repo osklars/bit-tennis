8ca8a2438527241b740c8a0384900a7052520560
Oskar <oskar.larsson@bitaddict.se>
2024-12-14 21:41:08 +0100

    Fix formatting

--- src/main/scala/StateManager.scala
+++ src/main/scala/StateManager.scala
@@ -3,15 +3,16 @@ import cats.syntax.all.*
 import fs2.Stream
 import fs2.concurrent.Topic
 
-class StateManager[F[_] : Concurrent](
-                                       state: Ref[F, List[GameState]],
-                                       updates: Topic[F, List[GameState]]
-                                     ):
+class StateManager[F[_] : Concurrent]
+(
+  state: Ref[F, List[GameState]],
+  updates: Topic[F, List[GameState]]
+):
   def process(event: GameEvent): F[GameState] = for
     states <- state.get
     currentState = states.headOption.getOrElse(GameState.initial(Player.A))
     newState = currentState.process(event)
-    newHistory = (newState :: states).take(5)  // Keep last 5 states
+    newHistory = (newState :: states).take(5) // Keep last 5 states
     _ <- state.set(newHistory)
     _ <- updates.publish1(newHistory)
   yield newState
